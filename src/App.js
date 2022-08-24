1528c41bbbd04bb681024660f7b304df66b56492
abrusu-collins <snillochemingson@gmail.com>
2022-08-24 23:30:03 +0000

    prototype

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./ind.css"
+import img from "./img/hero- img.png"
+function App() {
+  return (
+    
+  <main>
+  <img src={img} alt="kml"></img>
+  </main>
+  
+  );
+}
+
+export default App;
