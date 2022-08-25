77a1bb06511be4a16efac78d7a4e8a32575ef6df
abrusu-collins <snillochemingson@gmail.com>
2022-08-25 23:25:33 +0000

    working on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,8 @@ function Hero() {
         <div className="hero">
             <div className="herotxt">
                 <h1>JSCheatSheets</h1>
-                <p>Stop wasting your time when writing code. Get all your JavaScript cheetsheats here</p>
+                <p>Stop wasting your time when writing code.</p>
+                <p>Get all your JavaScript cheetsheats here</p>
             </div>
             <img src={img} alt="hero-img" />
         </div>
