22cf4adb0245451c4a32e120d147ea4d7e226e56
abrusu-collins <snillochemingson@gmail.com>
2022-08-25 23:49:29 +0000

    hero almost done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
-import img from "../img/carbon.svg"
+import img from "../img/carbon.svg";
 function Hero() {
-    return ( 
-        <div className="hero">
-            <div className="herotxt">
-                <h1>JSCheatSheets</h1>
-                <p>Stop wasting your time when writing code.</p>
-                <p>Get all your JavaScript cheetsheats here</p>
-            </div>
-            <img src={img} alt="hero-img" />
-        </div>
-     );
+  return (
+    <div className="hero">
+      <div className="herotxt">
+        <h1>JSCheatSheets</h1>
+        <p>Stop wasting your time when writing code.</p>
+        <p>
+          JSCheatsheets help you make quick <br></br> references whenever you
+          are stranded.
+        </p>
+        <p>Get all your JavaScript cheetsheats here.</p>
+        <a href=" ">All CheatSheets</a>
+      </div>
+      <img src={img} alt="hero-img" />
+    </div>
+  );
 }
 
 export default Hero;
