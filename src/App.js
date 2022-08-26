1750fe3bf50306cbc864dfbafbba706ac8eda412
abrusu-collins <snillochemingson@gmail.com>
2022-08-26 00:07:38 +0000

    hero done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/index.css"
 import Hero from "./components/Hero";
 import Recentheader from "./components/Recentheader";
+import Arrow from "./components/Arrow";
 function App() {
   return (
  <>
  <Hero/>
+ <Arrow/>
  <Recentheader/>
  </>
   
---
+++ src/components/Arrow.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+function Arrow() {
+    return ( 
+        <div className="arrow">
+            <FontAwesomeIcon icon={faCircleArrowDown}  size="2x"/>
+        </div>
+     );
+}
+
+export default Arrow;
