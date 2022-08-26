e55e664c67fe4a69860e6a148e368b29522b47b4
abrusu-collins <snillochemingson@gmail.com>
2022-08-26 10:12:55 +0000

    Deleted some stuffs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./styles/index.css"
 import Hero from "./components/Hero";
-import Recentheader from "./components/Recentheader";
 import Arrow from "./components/Arrow";
 function App() {
   return (
  <>
  <Hero/>
  <Arrow/>
- <Recentheader/>
  </>
   
   );
--- src/components/Recentheader.jsx
+++
@@ -1,7 +0,0 @@
-function Recentheader() {
-    return ( 
-        <h1 className="header">Recent CheatSheets</h1>
-     );
-}
-
-export default Recentheader;
