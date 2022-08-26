8de9bfbf34f98903cba6f1fcb76a683f6873a2a4
abrusu-collins <snillochemingson@gmail.com>
2022-08-26 15:50:00 +0000

    slick

--- src/components/SimpleSlider.jsx
+++ src/components/SimpleSlider.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default class SimpleSlider extends Component {
     render() {
@@ -11,8 +13,8 @@ export default class SimpleSlider extends Component {
         slidesToScroll: 1
       };
       return (
-        <div>
-          <h2> Single Item</h2>
+        <div className="slider">
+
           <Slider {...settings}>
             <div>
               <h3>1</h3>
@@ -22,15 +24,6 @@ export default class SimpleSlider extends Component {
             </div>
             <div>
               <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
             </div>
           </Slider>
         </div>
--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -1,9 +1,13 @@
+import SimpleSlider from "./SimpleSlider";
 function Testimonies() {
-    return ( 
-        <div className="testimonies">
-            <h1 className="testimoniesheader">What Other <br />  Developers <br /> are saying</h1>
-        </div>
-     );
+  return (
+    <div className="testimonies">
+      <h1 className="testimoniesheader">
+        What Other <br /> Developers <br /> are saying
+      </h1>
+      <SimpleSlider />
+    </div>
+  );
 }
 
 export default Testimonies;
