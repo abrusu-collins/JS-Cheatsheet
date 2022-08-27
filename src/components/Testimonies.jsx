6ff7f720099598db239dc65833936d3c8251ffe9
abrusu-collins <snillochemingson@gmail.com>
2022-08-27 00:27:43 +0000

    deleted slick, testimonial works

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -1,11 +1,19 @@
-import SimpleSlider from "./SimpleSlider";
 function Testimonies() {
   return (
     <div className="testimonies">
       <h1 className="testimoniesheader">
         What Other <br /> Developers <br /> are saying
       </h1>
-      <SimpleSlider />
+
+      <div className="4flex">
+        <div className="2flex">
+          <div className="testimonial1"></div>
+
+          <div className="testimonial2"></div>
+        </div>
+
+        <div className="2flex"></div>
+      </div>
     </div>
   );
 }
