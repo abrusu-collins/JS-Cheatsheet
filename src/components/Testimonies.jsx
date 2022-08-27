2a2e172ea77c0f35435017133e1df3fabba8d6c0
abrusu-collins <snillochemingson@gmail.com>
2022-08-27 00:30:20 +0000

    testimonial works

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -7,7 +7,15 @@ function Testimonies() {
 
       <div className="4flex">
         <div className="2flex">
-          <div className="testimonial1"></div>
+          <div className="testimonial1">
+            <div className="maintestimony">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Repudiandae architecto eaque esse unde recusandae amet dicta hic
+              alias sapiente, possimus odio rem quae omnis fugiat, magnam vel ea
+              in? Asperiores.
+            </div>
+            <div className="profile"></div>
+          </div>
 
           <div className="testimonial2"></div>
         </div>
