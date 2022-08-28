c6c24ad3a9c0b20f7308ce39e3999669ba78cec9
abrusu-collins <snillochemingson@gmail.com>
2022-08-28 13:26:37 +0000

    footer works

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
 function Footer() {
-    return ( 
-        <footer className="footer">
-            
-        </footer>
-     );
+  return (
+    <footer className="footer">
+      <div className="about">
+        <h1>Developed By : Abrusu Collins</h1>
+        <div className="about">
+          <h1>About</h1>
+          <p>
+            I created JSCheatsheets to help developers develop faster. It helps
+            make quick references whenever you forget something. It can also
+            serve as a quick revision guide whenever you want to revise any
+            JavaScript concept.
+
+          </p>
+        </div>
+      </div>
+      <div className="support">
+        <h1>Support</h1>
+        <p>Do you like JSCheatSheets, You can support the project to keep it going</p>
+        <a href=""> Support</a>
+      </div>
+
+      <div className="contacts">
+        <a href="">Twitter</a>
+        <a href=""> LinkedIn</a>
+        <a href="">GitHub</a>
+        <a href="">Feedback</a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
