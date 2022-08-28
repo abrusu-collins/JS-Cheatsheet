f362f8561e4f2aa13b5e7fd3dcd29cb904282567
abrusu-collins <snillochemingson@gmail.com>
2022-08-28 17:10:01 +0000

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./styles/index.css";
 import Home from "./components/Home";
+import Allcheatsheets from "./components/Cheatsheets/Allcheatsheets";
+import API from "./components/Cheatsheets/API/API";
+import Array from "./components/Cheatsheets/Array/Array";
+import DOM from "./components/Cheatsheets/DOM/DOM";
+import String from "./components/Cheatsheets/String/String";
+import RegExp from "./components/Cheatsheets/RegExp/RegExp";
+
 function App() {
   return (
     <>
      <Home/>
+ 
     </>
   );
 }
