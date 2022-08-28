1ab46d258420d57089eec41e4c6fe178e0048482
abrusu-collins <snillochemingson@gmail.com>
2022-08-28 15:51:58 +0000

    starting router

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,15 +17,15 @@ function Footer() {
       <div className="support">
         <h1>Support</h1>
         <p>Do you like JSCheatSheets, <br /> You can support     the <br /> project to keep it going</p>
-        <a href=""> Support</a>
+        <a href=" "> Support</a>
       </div>
 
       <div className="contacts">
         <h1>Contacts</h1>
-        <a href="">Twitter</a>
-        <a href=""> LinkedIn</a>
-        <a href="">GitHub</a>
-        <a href="">Feedback</a>
+        <a href=" ">Twitter</a>
+        <a href=" "> LinkedIn</a>
+        <a href=" ">GitHub</a>
+        <a href=" ">Feedback</a>
       </div>
     </footer>
   );
