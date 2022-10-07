2ca6a69bd390d0826c04f05029968ea9ae11049d
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 15:32:34 +0000

    cool

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -56,6 +56,30 @@ function Array() {
             We access elements/members of an array using their index. Indices
             start from 0(the first member), 1(the second member), 2, 3,..., n(last member).
           </p>
+          <div className="code_block">
+            <pre>
+              {" "}
+              <span className="keyword">let</span>{" "}
+              <span className="variable_name">array</span> ={" "}
+              <span className="keyword">new</span>{" "}
+              <span className="keyword">Array</span>{" "}
+              <span className="punctuation">(</span> 1
+              <span className="punctuation">,</span>2
+              <span className="punctuation">,</span>3{" "}
+              <span className="punctuation">)</span>{" "}
+              <span className="punctuation">;</span>
+            </pre>
+            <button
+              onClick={(e) => {
+                navigator.clipboard.writeText(
+                  "let array = new Array( 1,2,3 );"
+                );
+                e.currentTarget.innerText = "Copied";
+              }}
+            >
+              Copy
+            </button>
+          </div>
         </div>
       </div>
     </>
