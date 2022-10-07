79845760ba1fbecebbc132149c7a56b61d11849e
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 15:23:57 +0000

    progress

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -9,12 +9,13 @@ function Array() {
           <div className="code_block">
             <pre>
               <span className="keyword">let</span>{" "}
-              <span className="variable_name">array</span>{" "}={" "}
-              <span className="punctuation">[</span>{" "}1
+              <span className="variable_name">array</span> ={" "}
+              <span className="punctuation">[</span> 1
               <span className="punctuation">,</span>2
               <span className="punctuation">,</span>3
               <span className="punctuation">,</span>4{" "}
-              <span className="punctuation">]</span>;
+              <span className="punctuation">]</span>{" "}
+              <span className="punctuation">;</span>
             </pre>
             <button
               onClick={(e) => {
@@ -28,23 +29,33 @@ function Array() {
 
           <div className="code_block">
             <pre>
+              {" "}
               <span className="keyword">let</span>{" "}
-              <span className="variable_name">array</span>{" "}={" "}
-              <span className="punctuation">[</span>{" "}1
+              <span className="variable_name">array</span> ={" "}
+              <span className="keyword">new</span>{" "}
+              <span className="keyword">Array</span>{" "}
+              <span className="punctuation">(</span> 1
               <span className="punctuation">,</span>2
-              <span className="punctuation">,</span>3
-              <span className="punctuation">,</span>4{" "}
-              <span className="punctuation">]</span>;
+              <span className="punctuation">,</span>3{" "}
+              <span className="punctuation">)</span>{" "}
+              <span className="punctuation">;</span>
             </pre>
             <button
               onClick={(e) => {
-                navigator.clipboard.writeText("let array = [ 1,2,3,4 ];");
+                navigator.clipboard.writeText(
+                  "let array = new Array( 1,2,3 );"
+                );
                 e.currentTarget.innerText = "Copied";
               }}
             >
               Copy
             </button>
           </div>
+          <h2>Accessing Array Elements</h2>
+          <p>
+            We access elements/members of an array using their index. Indices
+            start from 0(the first member), 1(the second member), 2, 3,..., n(last member).
+          </p>
         </div>
       </div>
     </>
