94732809bbfbe1a1ce58238ed38953039ce205cc
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 13:23:08 +0000

    poppins tryout

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -2,16 +2,19 @@ function Array() {
   return (
     <>
       <div className="array">
+        <h1>Array Declaration</h1>
         <div className="banner_head">Array Cheatsheet</div>
       </div>
-      <button
-        onClick={() => {
-          navigator.clipboard.writeText("Copy this text to clipboard");
-          this.innerText = "Copied";
-        }}
-      >
-        Copy
-      </button>
+      <div className="declare_array">
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText("Copy this text to clipboard");
+            this.innerText = "Copied";
+          }}
+        >
+          Copy
+        </button>
+      </div>
     </>
   );
 }
