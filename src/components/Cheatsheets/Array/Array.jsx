abff4e7ff0c355690acf8de09be6328ada1c837f
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 13:13:49 +0000

    clipboard

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -1,13 +1,19 @@
 function Array() {
-    return ( 
-        <>
-        <div className="array">
-            <div className="banner_head">Array Cheatsheet</div>
-        </div>
-        
-        
-        </>
-     );
+  return (
+    <>
+      <div className="array">
+        <div className="banner_head">Array Cheatsheet</div>
+      </div>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText("Copy this text to clipboard");
+          this.innerText = "Copied";
+        }}
+      >
+        Copy
+      </button>
+    </>
+  );
 }
 
 export default Array;
