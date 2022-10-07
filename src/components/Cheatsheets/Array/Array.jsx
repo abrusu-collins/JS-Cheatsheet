3de54b5469536e4db114cd28cbd6ad7943584fa9
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 13:47:58 +0000

    making sense

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -3,18 +3,29 @@ function Array() {
     <>
       <div className="array">
         <div className="banner_head">Array Cheatsheet</div>
-      
-      <div className="declare_array">
-      <h2>Array Declaration</h2>
-        <button
-          onClick={() => {
-            navigator.clipboard.writeText("Copy this text to clipboard");
-            this.innerText = "Copied";
-          }}
-        >
-          Copy
-        </button>
-      </div>
+
+        <div className="declare_array">
+          <h2>Array Declaration</h2>
+          <div className="code_block">
+            <pre>
+              <span className="keyword">let</span>{" "}
+              <span className="variable_name">array</span>{" "}={" "}
+              <span className="punctuation">[</span>{" "}1
+              <span className="punctuation">,</span>2
+              <span className="punctuation">,</span>3
+              <span className="punctuation">,</span>4{" "}
+              <span className="punctuation">]</span>;
+            </pre>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText("Copy this text to clipboard");
+                this.innerText = "Copied";
+              }}
+            >
+              Copy
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
