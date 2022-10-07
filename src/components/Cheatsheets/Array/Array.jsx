90908917b14f6f9e21572210ed98835ec60b67d8
abrusu-collins <snillochemingson@gmail.com>
2022-10-07 14:14:13 +0000

    nice

--- src/components/Cheatsheets/Array/Array.jsx
+++ src/components/Cheatsheets/Array/Array.jsx
@@ -17,9 +17,29 @@ function Array() {
               <span className="punctuation">]</span>;
             </pre>
             <button
-              onClick={() => {
+              onClick={(e) => {
                 navigator.clipboard.writeText("let array = [ 1,2,3,4 ];");
-                this.innerText = "Copied";
+                e.currentTarget.innerText = "Copied";
+              }}
+            >
+              Copy
+            </button>
+          </div>
+
+          <div className="code_block">
+            <pre>
+              <span className="keyword">let</span>{" "}
+              <span className="variable_name">array</span>{" "}={" "}
+              <span className="punctuation">[</span>{" "}1
+              <span className="punctuation">,</span>2
+              <span className="punctuation">,</span>3
+              <span className="punctuation">,</span>4{" "}
+              <span className="punctuation">]</span>;
+            </pre>
+            <button
+              onClick={(e) => {
+                navigator.clipboard.writeText("let array = [ 1,2,3,4 ];");
+                e.currentTarget.innerText = "Copied";
               }}
             >
               Copy
